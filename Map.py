fb12942272aebb36732564d72419ff40d32c5e23
egulk9 <egulk9@gmail.com>
2020-05-20 00:52:12 +0200

    22:23

--- Map.py
+++ Map.py
@@ -1,14 +1,53 @@
 import sys
 
-x = 0
-print(sys.argv[1])
-for line in sys.argv:
-    print(line)
+#Eine implementierte Version der Karte. Ein Kartenobjekt speichert alle Straßen und deren Anzahl.
+class Map:
+
+    #Der Standardkonstruktor erzeugt ein Kartenobjekt indem er die eine input-Liste einliest und diese in Straßen
+    #konvertiert.
+    def __init__(self, input):
+        self.input = input
+        self.street_list = []
+        self.street_count = int(self.input[0][0])
+
+        for i in range(3, self.street_count-1):
+            print(i)
+            self.add_street(Street(Kreuzung(input[i][0]), Kreuzung(input[i][1])))
+
+    #Eine Straße wird der dem Attribut "Straßenliste" hinzugefügt
+    def add_street(self, street):
+        self.street_list.append(street)
 
 
-class Streets:
+#Ein Objekt, dass den Start und Endpunkt, sowie die Länge einer Straße speichert
+class Street:
 
-    def __init__(self, start_end_point,length):
+    #Der Standardkonstruktor gibt ein Straßenobjekt zurück.
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
 
-        self.point = start_end_point
-        self.length = length
+
+#Eine Kreuzung beschreibt einen Punkt, an dem eine Straße aufhört und eine weitere beginnt
+class Kreuzung:
+
+    #Der Standardkonstruktor wandelt einen String in Form (x,y) in ein Kreuzungsobjekt um.
+    def __init__(self, way_string):
+        self.x = int(way_string[1])
+        self.y = int(way_string[3])
+
+
+#Öffnen der Datei
+with open("abbiegen0.txt") as f:
+    input = f.readlines()
+
+#Entfernen der Linebreaks
+altinput = [x[:-1] for x in input]
+
+#Aufteilen der Kreuzungen
+input =  [x.split(" ") for x in altinput]
+
+#Erzeugen der Karte
+map1 = Map(input)
+
+print("done")
